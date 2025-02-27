b2bac4969ebbd23360725162f0a3bf94cf56d4df
Vincent Jared <122254590+jaredvincent414@users.noreply.github.com>
2025-02-27 13:44:43 -0500

    Update WordGenerator.swift

--- Wordle/WordGenerator.swift
+++ Wordle/WordGenerator.swift
@@ -11,11 +11,15 @@ class WordGenerator {
   // Exercise 8: Add more possible words and return a **random** string every time `generateRandomWord` is called
   // IMPORTANT: MAKE SURE ALL THE LETTERS IN THE NEW WORDS YOU ADD ARE CAPITALIZED (e.g. "AUDIO" instead of "audio")
   // Tip: Look at the Array API documentation to see what method returns a random element
-  // Checkpoint: After finishing this exercise, you should now have a different goal word each time you run the app! Try printing out the selected goal word to check. If it's not working, check that you have done this exercise correctly.
-  static let possibleWords = ["ABCDE"]
+  // Checkpoint: After finishing this exercise, you should now have a different goal word each time you run the app!
+  Try printing out the selected goal word to check. If it's not working, check that you have done this exercise correctly.
+  static let possibleWords = ["ABCDE", "CHESS", "DOORS", "EGYPT", "FLAME", "GLOBE", "HAPPY","JOKES", "KITES", "LIONS", "MANGO", "NINJA", "OCEAN", 
+                              "PIZZA", "QUEEN", "ROBOT","SUNNY", "TIGER", "ULTRA", "VIBES", "WATER", "XENON", "YOUTH", "ZEBRA","VIRUS", "PLANET", 
+                              "STONE", "NORTH", "EAGLE","PEACH","LIGHT", "GIRLS", "STORM", "BLOOM", "CROWN", "CLOUD","STARS", "MUSIC", "BRAIN", 
+                              "NATURE", "JUMPY", "FRAME"]
   static func generateRandomWord() -> String? {
     // START YOUR CODE HERE
-    return possibleWords.first
+    return possibleWords.randomElement()
     // END YOUR CODE HERE
   }
 }
